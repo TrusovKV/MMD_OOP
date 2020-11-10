378d4309c997dc99af0b573c419220da530dcedd
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2020-11-10 12:58:09 +0700

    Compile-time constructor integrates
    Integration is produced during the objects template construction

--- CompileTime_solver/CompileTime_solver/pch.h
+++ CompileTime_solver/CompileTime_solver/pch.h
@@ -23,14 +23,29 @@ private:
 	double bottom;
 	double top;
 	int pointsNumber;
+	int iterator;
 
 public:
 
 	constexpr CompileTimeSolver(double Down, double Up, int MeshDepth) : sum(0), step(0), currentPoint(0),
-		nextPoint(0), bottom(Down), top(Up), pointsNumber(MeshDepth)
+		nextPoint(0), bottom(Down), top(Up), pointsNumber(MeshDepth), iterator(0)
 	{}
+
+	constexpr CompileTimeSolver(double Down, double Up, int MeshDepth, double (*function)(double x)) : step((Up - Down) / (MeshDepth - 1)),
+		currentPoint(Down),	nextPoint(0), bottom(Down), top(Up), pointsNumber(MeshDepth), iterator(0), sum(0)
+	{
+		while (iterator < MeshDepth)
+		{
+			nextPoint = currentPoint + step;
+			sum += (function(currentPoint) + function(nextPoint));
+			currentPoint = nextPoint;
+			++iterator;
+		}
+		sum *= 0.5 * step ;
+	}
+
 	CompileTimeSolver() : sum(0), step(0), currentPoint(0),
-		nextPoint(0), bottom(0), top(0), pointsNumber(0)
+		nextPoint(0), bottom(0), top(0), pointsNumber(0), iterator(0)
 	{
 		cout << "Run-time constructor" << endl;
 	}
@@ -46,7 +61,10 @@ public:
 		}
 		return 0.5 * sum * step;
 	}
-
+	double tellSum()
+	{
+		return this->sum;
+	}
 
 };
 
@@ -55,6 +73,7 @@ class Output
 {
 private:
 	T2 arg;
+	T2 dynArg;
 public:
 
 	constexpr Output(T2& argument):arg(argument)
@@ -66,13 +85,17 @@ public:
 	{
 		this->arg = argument;
 	}
-	constexpr 	void getConstArg(T2 argument)
+	constexpr 	void getDyntArg(T2 argument)
 	{
-		this->arg = argument;
+		this->dynArg = argument;
 	}
 	void show()
 	{
 		cout << this->arg << endl;
+	}
+	void showDyn()
+	{
+		cout << this->dynArg << endl;
 	}
 };
 
--- CompileTime_solver/CompileTime_solver/test.cpp
+++ CompileTime_solver/CompileTime_solver/test.cpp
@@ -3,9 +3,10 @@
 
 int main() 
 {
-	 CompileTimeSolver<int> a(0, 10, 300);
+	 CompileTimeSolver<int> a(0, 10, 300), c(0, 10, 300, velosityProfile);
 	 Output<double> b;
-	 b.getConstArg(a.Integrate(velosityProfile));
+	 b.getArg(a.Integrate(velosityProfile));
+	 b.getDyntArg(c.tellSum());
 	 b.show();
-
+	 b.showDyn();
 }
