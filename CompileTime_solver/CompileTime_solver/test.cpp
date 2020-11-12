535de1599016c56d5970d8cab91abb9e1f5e2e82
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2020-11-12 13:00:53 +0700

    Update test.cpp

--- CompileTime_solver/CompileTime_solver/test.cpp
+++ CompileTime_solver/CompileTime_solver/test.cpp
@@ -1,12 +1,25 @@
 #include "pch.h"
 
-
-int main() 
+template <typename X> /*static const*/ double functon( /*static const*/ double x)
 {
+	return 0.3 * x * x + 2.0 * x - 1.0;
+}
+TEST(check_polynom, check_polynom) {
+	EXPECT_EQ(1.3, functon<double>(1.0));
+}
+
+
+int main(int argc, char** argv)
+{
+
+	//___________________________________________________________________
 	 CompileTimeSolver<int> a(0, 10, 300), c(0, 10, 300, velosityProfile);
 	 Output<double> b;
 	 b.getArg(a.Integrate(velosityProfile));
 	 b.getDyntArg(c.tellSum());
 	 b.show();
 	 b.showDyn();
+	 cout<< "hh"<<endl;
+	 testing::InitGoogleTest(&argc, argv);
+	 return RUN_ALL_TESTS();
 }
