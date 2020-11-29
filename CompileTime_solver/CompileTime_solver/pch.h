84c49ca02a2dbe028b9409360394efe275a7bf10
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2020-11-29 21:47:32 +0700

    Done!

--- CompileTime_solver/CompileTime_solver/pch.h
+++ CompileTime_solver/CompileTime_solver/pch.h
@@ -5,102 +5,48 @@
 #include <cmath>
 #include <math.h>
 #include <gtest/gtest.h>
-
+#include <cstdint>
 using namespace std;
 
-constexpr double velosityProfile(double x) {
-	return 0.3 * x * x + 2 * x - 1;
+template<int64_t N >
+struct function {
+	static const int64_t val = static_cast <const unsigned>(100 * (0.24 + 0.5 * N + 2.33 * N * N - 0.02 * N * N * N));
+};
+
+TEST(checkFunc, checkFunc) {
+	EXPECT_EQ(24, function<0>::val);
 }
 
-template <typename T1>
-class CompileTimeSolver
-{
-private:
-	double sum;
-	double step;
-	double currentPoint;
-	double nextPoint;
-	double bottom;
-	double top;
-	int pointsNumber;
-	int iterator;
-
-
-public:
-
-	constexpr CompileTimeSolver(double Down, double Up, int MeshDepth) : sum(0), step(0), currentPoint(0),
-		nextPoint(0), bottom(Down), top(Up), pointsNumber(MeshDepth), iterator(0)
-	{}
-
-	constexpr CompileTimeSolver(double Down, double Up, int MeshDepth, double (*function)(double x)) : step((Up - Down) / (MeshDepth - 1)),
-		currentPoint(Down),	nextPoint(0), bottom(Down), top(Up), pointsNumber(MeshDepth), iterator(0), sum(0)
-	{
-		while (iterator < MeshDepth)
-		{
-			nextPoint = currentPoint + step;
-			sum += (function(currentPoint) + function(nextPoint));
-			currentPoint = nextPoint;
-			++iterator;
-		}
-		sum *= 0.5 * step ;
-	}
-
-	CompileTimeSolver() : sum(0), step(0), currentPoint(0),
-		nextPoint(0), bottom(0), top(0), pointsNumber(0), iterator(0)
-	{
-		cout << "Run-time constructor" << endl;
-	}
-	constexpr double Integrate(double (*function)(double x))
-	{
-		step = (top - bottom) / (pointsNumber - 1);
-		currentPoint = bottom;
-		for (int i = 0; i < pointsNumber; ++i)
-		{
-			nextPoint = currentPoint + step;
-			sum += (function(currentPoint) + function(nextPoint));
-			currentPoint = nextPoint;
-		}
-		return 0.5 * sum * step;
-	}
-	double tellSum()
-	{
-		return this->sum;
-	}
-
+template<int64_t N >
+struct partSum {
+	static const int64_t val = function<N+1>::val + function<N>::val;
 };
-
-template <typename T2>
-class Output
-{
-private:
-	T2 arg;
-	T2 dynArg;
-public:
-
-	constexpr Output(T2& argument):arg(argument)
-	{}
-
-	constexpr Output()
-	{}
-	void getArg(T2 argument)
-	{
-		this->arg = argument;
-	}
-	constexpr 	void getDyntArg(T2 argument)
-	{
-		this->dynArg = argument;
-	}
-	void show()
-	{
-		cout << this->arg << endl;
-	}
-	void showDyn()
-	{
-		cout << this->dynArg << endl;
-	}
+TEST(checkPS, checkPS) {
+	EXPECT_EQ(329, partSum<0>::val);
+}
+template<int64_t N>
+struct int1d {
+	static const int64_t value = partSum<N>::val + int1d<N - 1>::value;
 };
+template<>
+struct int1d<0> {
+	static const int64_t value = partSum<0>::val;
+};
+TEST(checkU1dInt0, checkU1dInt0) {
+	EXPECT_EQ(329, int1d<0>::value);
+}
 
-//template <typename T2>
-//class Output<T2>::	constexpr Output(T2& argument) :arg(argument)
-//{}
-//#include "Output.cpp"
+TEST(checkU1dInt, checkU1dInt) {
+	EXPECT_EQ(1674, int1d<1>::value);
+}
+
+template<int64_t N, int8_t step, int8_t order>
+struct integrate1d{
+static const double integ1d() {
+	return 0.005*int1d<N>::value*( static_cast <const double>(step)/static_cast <const double>(order) );
+};
+};
+TEST(checkU1dInteg, checkU1dInteg) {
+	EXPECT_EQ(837, static_cast <const unsigned> (100 * integrate1d<1, 1, 1>::integ1d()));
+}
+
--- CompileTime_solver/CompileTime_solver/test.cpp
+++ CompileTime_solver/CompileTime_solver/test.cpp
@@ -1,25 +1,12 @@
 #include "pch.h"
 
-template <typename X> /*static const*/ double functon( /*static const*/ double x)
-{
-	return 0.3 * x * x + 2.0 * x - 1.0;
-}
-TEST(check_polynom, check_polynom) {
-	EXPECT_EQ(1.3, functon<double>(1.0));
-}
 
 
 int main(int argc, char** argv)
 {
-	int aff = 5*5;
-	//___________________________________________________________________
-	 CompileTimeSolver<int> a(0, 10, 300), c(0, 10, 300, velosityProfile);
-	 Output<double> b;
-	 b.getArg(a.Integrate(velosityProfile));
-	 b.getDyntArg(c.tellSum());
-	 b.show();
-	 b.showDyn();
-	 cout<< "hh"<<endl;
-	 testing::InitGoogleTest(&argc, argv);
-	 return RUN_ALL_TESTS();
+	//integrate1d<N, a, b > <=> integrate from 0 to [N +1]*a/b
+	const double f5 = integrate1d<10, 1, 1 >::integ1d();
+	cout << f5 << endl;
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
