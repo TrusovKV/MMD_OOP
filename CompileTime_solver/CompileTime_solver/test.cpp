169b60ad67cef8506a5c020553fbd6ca01dac30a
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2020-12-03 12:24:58 +0700

    Update test.cpp

--- CompileTime_solver/CompileTime_solver/test.cpp
+++ CompileTime_solver/CompileTime_solver/test.cpp
@@ -4,7 +4,6 @@
 
 int main(int argc, char** argv)
 {
-	//integrate1d<N, a, b > <=> integrate from 0 to [N +1]*a/b
 	const double f5 = integrate1d<10, 1, 1 >::integ1d();
 	cout << f5 << endl;
 	testing::InitGoogleTest(&argc, argv);
