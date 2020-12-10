52665ff180da170c2da047a33c7f9fcbdd60333a
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2020-12-10 11:40:53 +0700

    Update test.cpp

--- CompileTime_solver/CompileTime_solver/test.cpp
+++ CompileTime_solver/CompileTime_solver/test.cpp
@@ -2,7 +2,7 @@
 
 
 static const double Add(int x, int y)
-{
+{ // Useless
 	__asm
 	{
 		mov eax, x;
@@ -12,7 +12,7 @@ static const double Add(int x, int y)
 	}
 	return x;
 }
-
+//Constexpr polynom
 constexpr double polynom(double N) {
 	return 0.24 + 0.5 * N + 2.33 * N * N - 0.02 * N * N * N;
 }
@@ -20,7 +20,7 @@ constexpr double polynom(double N) {
 TEST(check_polynom, check_polynom) {
 	EXPECT_EQ(24, static_cast<int>(100*polynom(0) ) );
 }
-
+// Constexpr Integration
 constexpr double Integrate(double step, int32_t points_number, double (*function)(double x)) {
 	double sum = 0;
 	double current_point = 0;
@@ -42,13 +42,17 @@ TEST(Integration, Integration) {
 
 int main(int argc, char** argv)
 {
+	// Useless
 	/*static*/ const int i = 0;
 	_asm {
 		mov i, 2333
 	}
+
 	//static_assert (i == 2333);
 	static const double f5 =  integrate1d<10, 1, 1 >::integ1d();
 	static const int f0 = intT1d<10>::value;
+
+	// Template-based calculations
 	static const int f6 =  int1d<10>::value;
 	static const int f1 = int1d<1>::value;
 	static const int g = f6 * 0.005;//997.095;
@@ -58,9 +62,12 @@ int main(int argc, char** argv)
 	static_assert(f0 == 199419/*997.095*/);
 	static_assert(f1 == 1674, "int must be 4 bytes");
 	static_assert(f6 == 199419, "int must be 4 bytes");
-	//static_assert( H == h);
+	
+	// Successfull static assert
 	static_assert(997.095 == f6*0.005);
 	static_assert(997/*.095*/ == g);
+
+	// Constexpr static assert
 
 	constexpr auto integral = Integrate(1, 11, polynom);
 	static_assert(997.095 == integral);
