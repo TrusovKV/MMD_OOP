f335c00ad99f174f8dd0272dc7b61ebedcab8072
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2021-04-03 09:33:41 +0700

    sasas

--- SCVParser_Mk1/SCVParser_Mk1/LazyIterator.h
+++ SCVParser_Mk1/SCVParser_Mk1/LazyIterator.h
@@ -9,19 +9,17 @@ template<typename...Args>
 class csv_parser_iterator
 {
 private:
-	std::ifstream& in;// объект файла
-	uint64_t column_count = sizeof...(Args);// ожидаемое количество колонок, это количество полученных параметров шаблона
-	int64_t current_row;//текущая строка
-	SCVCustomizer & m_settings;//объект настроек
-	std::tuple<Args...> buff;//объект с текущими прочитанными данными(одна строка)
+	std::ifstream& in;
+	uint64_t column_count = sizeof...(Args);
+	int64_t current_row;
+	SCVCustomizer & m_settings;
+	std::tuple<Args...> buff;//row
 		
-	// метод чтения строки из файла
+
 	std::tuple<Args...> row_parse();
 
-	// вспомогательный метод для make_tuple_impl
 	auto make_tuple_(std::vector<std::string> const& data);
 
-	// метод для формирования tuple из набора данных в строковом виде
 		template<typename T, std::size_t... I>
 	auto make_tuple_impl(T const& data, std::index_sequence<I...>)
 	{
@@ -29,38 +27,34 @@ private:
 	}
 public:
 	csv_parser_iterator(std::ifstream& file_obj, SCVCustomizer& settings, bool end = false);
-	// оператор префиксного инкремента 
+
 	csv_parser_iterator& operator++()
 	{
 		if (current_row == -1)
 			throw std::runtime_error("End iterator not changing");
-		// читаем следующую строку из файла 
 		buff = row_parse();
 		return *this;
 	}
-	// оператор постфиксного инкремента
+
 	csv_parser_iterator& operator++(int)
 	{
 		if (current_row == -1)
 			throw std::runtime_error("End iterator not changing");
-		// читаем следующую строку из файла
 		buff = row_parse();
 	}
-	// оператор разименования
+ 
 	std::tuple<Args...>& operator*()
 	{
 		if (current_row == -1)
 			throw std::runtime_error("Operator \"*\" not used for end iterator");
-		// возвращаем текущие прочитанные данные
 		return buff;
 	}
-	// оператор сравнения итераторов
+
 	bool operator!=(csv_parser_iterator const& r);
 
-	// опретор каста итератора к tuple
 	operator std::tuple<Args...>& ()
 	{
-		return buff;
+		return buff; // cust it to tuple
 	}
 };
 
--- SCVParser_Mk1/SCVParser_Mk1/SCVParserEngine.h
+++ SCVParser_Mk1/SCVParser_Mk1/SCVParserEngine.h
@@ -9,27 +9,20 @@
 #include "LazyIterator.h"
 
 
-// класс csv парсера
-// реализует ленивое чтение из файла, то есть чтение происходит лишь при запросе
 template<typename... Args>
 class csv_parser
 {
 public:
-	// задаём итератор для того чтобы работать с файлом 
+
 	using iterator = csv_parser_iterator<Args...>;
-	// консруктор принимает на вход:
-	// file_obj - объект файла
-	// num_skip_rows - количество строк которое необходимо пропустить 
-	// settings - объект настройки
+
 	csv_parser(std::ifstream& file_obj, int64_t num_skip_rows, SCVCustomizer settings = SCVCustomizer()) :
 		in(file_obj),
 		m_settings(settings)
 	{
-		// проверяем открыт ли файл
 		if (!file_obj)
-			throw std::runtime_error("File not open");// если нет, то выбросим исключение
+			throw std::runtime_error("File not open");
 
-		// пропустим то количество строк, которое было передано в конструстор
 		std::string buff;
 		for (; num_skip_rows > 0; --num_skip_rows)
 		{
@@ -37,33 +30,31 @@ public:
 			in.ignore(max_line, settings.tellNewLineSymb());
 		}
 	}
-	// деструктор
+
 	~csv_parser()
 	{
-		// закрываем файл
 		in.close();
 	}
 
-	// метод возвращает итератор на начало
 	iterator begin()
 	{
 		in.seekg(in.beg);
 		return iterator(in, m_settings);
 	}
-	// метод возвращает итератор на конец
+
 	iterator end()
 	{
 		return iterator(in, m_settings, true);
 	}
-	// вовращает позицию чтения в начало файла
+
 	void reset()
 	{
 		in.seekg(in.beg);
 	}
 
 private:
-	std::ifstream& in;// объект файла
-	SCVCustomizer m_settings;// настройки
+	std::ifstream& in;
+	SCVCustomizer m_settings;
 };
 
 template <size_t n, typename... T>
