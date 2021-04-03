9c1706fd530cfe8dfeb134212206cf118e4ea8be
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2021-04-03 11:17:53 +0700

    hh

--- SCVParser_Mk1/SCVParser_Mk1/SCVParser_Mk1.cpp
+++ SCVParser_Mk1/SCVParser_Mk1/SCVParser_Mk1.cpp
@@ -2,8 +2,12 @@
 #include <iostream>
 #include "SCVParserEngine.h"
 
+using namespace std;
+
 int main()
 {
-    
+    ifstream file_obj("D:\\GitHub\\MMD_OOP\\SCVParser_Mk1\\SCVParser_Mk1\\test.txt");
+   // file_obj = ;
+    csv_parser<int, string, double> sp( file_obj, 0, SCVCustomizer());
 }
 
