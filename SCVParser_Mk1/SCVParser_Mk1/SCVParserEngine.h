8e55e6bb87d1d3889482d2a8ebc54dd7b1fec5dc
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2021-04-29 18:51:33 +0700

    dsd
    ddd

--- SCVParser_Mk1/SCVParser_Mk1/SCVParserEngine.h
+++ SCVParser_Mk1/SCVParser_Mk1/SCVParserEngine.h
@@ -12,7 +12,6 @@ class Engine
 private:
 	ifstream input;
 	parserSCV<int, string> parser;
-
 	SCVCustomizer cust;
 public:
 	Engine() : input("D:\\GitHub\\MMD_OOP\\SCVParser_Mk1\\test.csv"), parser(input)
