5173f6aae729315a397eb6ecbe95e08f05ef6742
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2021-04-30 12:32:41 +0700

    Update SCVParser_Mk1.cpp

--- SCVParser_Mk1/SCVParser_Mk1/SCVParser_Mk1.cpp
+++ SCVParser_Mk1/SCVParser_Mk1/SCVParser_Mk1.cpp
@@ -1,19 +1,10 @@
 ﻿#pragma once
-//#include <iostream>
-//#include "SCVCustomizer.h"
 #include "SCVParserEngine.h"
 
 using namespace std;
 
 int main()
 {
-    //ifstream input("D:\\GitHub\\MMD_OOP\\SCVParser_Mk1\\test.csv");
-    //parserSCV<int, string> parser(input);
-
-    //SCVCustomizer cust;
-
-
-    //parser.getAllElements();
     Engine engine;
     engine.start();
     return 0;
