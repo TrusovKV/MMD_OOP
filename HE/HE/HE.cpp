11de92fb0f48dda2b3900fd9055c866f8c92010a
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2021-06-12 11:31:57 +0700

    broken again

---
+++ HE/HE/HE.cpp
@@ -0,0 +1,39 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <string>
+
+#include "MyArchr.h"
+using namespace std;
+
+int main() //
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251); //Set encoding
+
+	string adres = "D:\\text\\voina_i_mir_first_tom.txt";
+	string final_name = "my_archive.txt";
+	char action;
+
+	cout << "Do you want pack(P) or unpack(u)?" << endl;
+	cin >> action;
+
+	myArchivator arch(adres, final_name);
+
+	if (action == 'P') {
+		// Packing //
+
+		arch.readTxt();
+		arch.BuildTree();
+		arch.HaffmansCode();
+		arch.writeToTxt();
+	}
+	else if (action == 'u') {
+		// Unpacking //
+		arch.GetInfo();
+		arch.UnPack();
+	}
+	else {
+		cout << "Please, try again" << endl;
+	}
+
+}
