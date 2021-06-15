255855abe7b95970a6230cd4cb2b9a08c287af9a
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2021-06-15 21:43:19 +0700

    added time check
    ctime

--- HuffmanEncoder/HuffmanEncoder/main.cpp
+++ HuffmanEncoder/HuffmanEncoder/main.cpp
@@ -1,8 +1,10 @@
 #pragma once
+#include <ctime>
 #include "Archiver.h"
 
 int main()
 {
+	time_t start, end;
 	Archivator arc;
 
 	cout << "-c == code  || -d == decode " << endl;
@@ -14,14 +16,19 @@ int main()
 		cout << endl;
 		if (ch == 'c')
 		{
+			time(&start);
 			arc.archive();
+			time(&end);
 			break;
 		}
 		if (ch == 'd')
 		{
+			time(&start);
 			arc.reArchive();
+			time(&end);
 			break;
 		}
 	}
+	cout << difftime(end, start) << endl;
 	return 0;
 }
