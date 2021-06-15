8c45c395fe68a030ba66b7bdb93155b239859504
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2021-06-15 17:09:10 +0700

    Update mainwindow.cpp

--- SodaProblem/mainwindow.cpp
+++ SodaProblem/mainwindow.cpp
@@ -23,19 +23,19 @@ void MainWindow::on_pushButton_clicked(bool checked)
     qreal step = 0.05; // to plotter
 
     Hub hb(
-                int N,
-                double x0,
-                double xN,
-                double t,
-                double gamma,
-                double p1,
-                double rho1,
-                double u1,
-                double p4,
-                double rho4,
-                double u4,
+                ui->N_text,
+                ui->From_text,
+                ui->To_text,
+                ui->Time_text,
+                ui->Adibatic_text,
+                ui->p1_text,
+                ui->rho1_text,
+                ui->v1_text,
+                ui->p2_text,
+                ui->rho2_text,
+                ui->v2_text,
 
-                double NumDiv,
-                double st);
+                numberDivisions,
+                step);
 }
 
