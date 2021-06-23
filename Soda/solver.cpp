32756a1f3ea02899b636cb34ed6863c553d60735
TrusovKV <70829218+TrusovKV@users.noreply.github.com>
2021-06-23 17:51:39 +0700

    Update solver.cpp

--- Soda/solver.cpp
+++ Soda/solver.cpp
@@ -52,5 +52,26 @@ void Solver::RCS(){
     p2 = 1;
     p3 = 0.5;
 
+
+    while (abs(p2 - p3) > tol) {
+        //first family R
+        a2 = a1 + delta * (uL - u);
+        p2 = pL * pow(a2 / a1, alfa);
+        dp2 = -gamma * p2 / a2;
+
+        //second family S
+        V4 = (gamma + 1) / 4 * (u - uR) + sqrt(pow((gamma + 1) / 4, 2) * (u - uR) * (u - uR) + a4 * a4);
+        M4 = V4 / a4;
+        p3 = pR * (1 + (2 * gamma) / (gamma + 1) * (M4 * M4 - 1));
+        a3 = a4 * sqrt((gamma + 1 + (gamma - 1) * (p3 / pR)) / (gamma + 1 + (gamma - 1) * (pR / p3)));
+        wd = uR - V4;
+        dp3 = 2 * gamma * (pR / a4) * (pow((abs(M4)), 3) / (1 + M4 * M4));
+
+        //New velocity and update of the iteration
+        u -= (p2 - p3) / (dp2 - dp3);
+
+        iter++;
+    }
+
 }//EOF solve()
 
